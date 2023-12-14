e2189d883671fd7f8119a127fe0269e47e8e1940
Anasteisher <147333717+Anasteisher@users.noreply.github.com>
2023-12-14 07:23:07 +0300

    Add files via upload

---
+++ testSite/about/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Product
+
+MENU = {"главная":"/", "о нас":"/about", "блог":"/blog", "отзывы":"/comment"}
+
+def about_page(request):
+    products = Product.objects.all()
+    title = "О нас"
+    data = {"menu":MENU,"title":title, "products": products}
+    return render(request, "./about.html", context=data)
+
+
+def comment_page(request):
+    comment = DogComment.objects.filter(checkbox=True)
+    title = "Добавить отзыв"
+    data = {"menu":MENU,"title":title, "products": products}
+    return render(request, "./comment.html", context=data)
+
+def thanks_page(request):
+    user_name = request.POST['user_name']
+    email = request.POST['email']
+    feedback = request.POST['feedback']
+    checkbox = request.POST.get('checkbox', False)
+    DogComment.objects.create(user_name=user_name, email=email, feedback=feedback, checkbox=checkbox)
+    title = "Страница виляющих хвостиков"
+    data = {"menu":MENU,"title":title, "user_name":user_name}
+    return render(request, "./thanks.html", context=data)
